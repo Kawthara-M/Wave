1e1442de8d0b0c168422a8cfcf70d2fd26eafa3e
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-07-17 15:49:02 +0300

    delete

--- db/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-mongoose.connect(process.env.MONGODB_URI)
-
-mongoose.connection.on("connected", () => {
-  console.log(`Successfully connected to MongoDB database . . .`)
-})
-
-module.exports = mongoose
