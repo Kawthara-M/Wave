696dc5028a3862a9a6cb309f561ed037b0638f3a
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-07-20 09:44:46 +0300

    post routers are ready

---
+++ routes/postRouter.js
@@ -0,0 +1,17 @@
+const router = require("express").Router()
+const postCtrl = require("../controllers/postController")
+const multer = require("multer")
+const upload = multer({ dest: "uploads/" })
+
+router.get("/new", postCtrl.post_create_get)
+router.post("/", postCtrl.post_create_post)
+
+router.get("/", postCtrl.post_index_get)
+router.get("/:postId", postCtrl.post_show_get)
+
+router.get("/:postId/edit", postCtrl.post_edit_get)
+router.put("/:postId", postCtrl.post_update_put)
+
+router.delete("/:postId", postCtrl.post_delete_delete)
+
+module.exports = router
