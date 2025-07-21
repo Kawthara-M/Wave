c140de4333dded765e8940252b4fa6ecb85ad7ea
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-07-21 14:04:05 +0300

    all working on edit page issues

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,6 @@ exports.user_show_get = async (req, res) => {
     const user = await User.findById(req.params.id)
     const posts = await Post.find({ user: user._id })
 
-    console.log(user)
     const userData = {
       _id: user._id,
       username: user.username,
@@ -24,7 +23,8 @@ exports.user_show_get = async (req, res) => {
 
 exports.user_edit_get = async (req, res) => {
   try {
-    res.render("./users/edit.ejs", { data })
+      const currentUser = await User.findById(req.params.id)
+    res.render(`./users/edit.ejs`, currentUser)
   } catch (error) {
     console.error("An error has occurred rendering a profile!", error.message)
   }
@@ -32,15 +32,23 @@ exports.user_edit_get = async (req, res) => {
 
 exports.user_update_put = async (req, res) => {
   try {
-    const currentUser = await User.findById(req.session.user._id)
+    const currentUser = await User.findByIdAndUpdate(req.session.user._id,req.body,req.file.filename)
+     const user = await User.findById(req.session.user._id)
 
-    const post = currentUser.posts.id(req.params.postId)
+    const userData = {
+      _id: user._id,
+      username: user.username,
+      picture: user.profileImage,
+      birthday:user.birthday,
+      bio:user.bio,
+      posts: user.posts,
+    }
 
-    post.set(req.body)
+    //currentUser.set(req.body)
 
     await currentUser.save()
 
-    res.redirect(`/users/profile.ejs`)
+   res.render(`./users/profile.ejs`,{userData})
   } catch (error) {
     console.error("An error has occurred rendering a profile!", error.message)
   }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,5 +3,7 @@ const router = require('express').Router();
 const userController = require('../controllers/userController.js')
 
 router.get('/:id', userController.user_show_get);
+router.get('/:id/edit', userController.user_edit_get);
+router.put('/:id', userController.user_update_put);
 
 module.exports = router
