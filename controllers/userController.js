bb46275b909eead44fb6ee21e36edb20cf55ad58
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-07-21 20:42:30 +0300

    profile is somewhat styled

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,22 @@
 const User = require("../models/User.js")
 const Post = require("../models/Post.js")
+const { format } = require("date-fns")
 
 exports.user_show_get = async (req, res) => {
   try {
     const user = await User.findById(req.params.id)
     const posts = await Post.find({ user: user._id })
+    const formattedBirthday = user.birthday
+      ? format(new Date(user.birthday), "dd/MM/yyyy")
+      : null
+    console.log(formattedBirthday)
 
     const userData = {
       _id: user._id,
       username: user.username,
       picture: user.profileImage,
-      birthday:user.birthday,
-      bio:user.bio,
+      birthday: formattedBirthday,
+      bio: user.bio,
       posts: posts,
     }
 
@@ -23,7 +28,7 @@ exports.user_show_get = async (req, res) => {
 
 exports.user_edit_get = async (req, res) => {
   try {
-      const currentUser = await User.findById(req.params.id)
+    const currentUser = await User.findById(req.params.id)
     res.render(`./users/edit.ejs`, currentUser)
   } catch (error) {
     console.error("An error has occurred rendering a profile!", error.message)
@@ -32,24 +37,28 @@ exports.user_edit_get = async (req, res) => {
 
 exports.user_update_put = async (req, res) => {
   try {
-    const currentUser = await User.findByIdAndUpdate(req.session.user._id,req.body,req.file.profileImage)
-     const user = await User.findById(req.session.user._id)
+    const currentUser = await User.findByIdAndUpdate(
+      req.session.user._id,
+      req.body,
+      req.file.profileImage
+    )
+    const user = await User.findById(req.session.user._id)
     const posts = await Post.find({ user: user._id })
-
+    const formattedBirthday = user.birthday
+      ? format(new Date(user.birthday), "dd/MM/yyyy")
+      : null
     const userData = {
       _id: user._id,
       username: user.username,
       picture: user.profileImage,
-      birthday:user.birthday,
-      bio:user.bio,
+      birthday: formattedBirthday,
+      bio: user.bio,
       posts: posts,
     }
 
-    //currentUser.set(req.body)
-
     await currentUser.save()
 
-   res.render(`./users/profile.ejs`,{userData})
+    res.render(`./users/profile.ejs`, { userData })
   } catch (error) {
     console.error("An error has occurred rendering a profile!", error.message)
   }
