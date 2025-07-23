7e03a5c451f7b8d97d20cb44d074dff283d93e3c
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-07-23 12:51:09 +0300

    user can change profile photo

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -17,17 +17,17 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 router.get("/:id", userController.user_show_get)
-router.get("/:id/edit", userController.user_edit_get)
+router.get("/:id/edit", upload.single("profileImage"), userController.user_edit_get)
 router.put(
   "/:id",
   upload.single("profileImage"),
   userController.user_update_put
 )
-router.put(
-  "/:id",
-  upload.single("profileImage"),
-  userController.user_update_put
-)
+// router.put(
+//   "/:id",
+//   upload.single("profileImage"),
+//   userController.user_update_put
+// )
 router.post("/search", userController.user_search_post)
 
 module.exports = router
