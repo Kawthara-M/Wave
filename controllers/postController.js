c1a9d6b8da6d86b553ee2675670199650e839ee6
Malak1805 <mjma200018@gmail.com>
2025-07-23 11:32:54 +0300

    fixed liking feature final version

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,12 +1,12 @@
-const User = require("../models/User.js")
-const Post = require("../models/Post.js")
-const Comment = require("../models/Comment.js")
+const User = require('../models/User.js')
+const Post = require('../models/Post.js')
+const Comment = require('../models/Comment.js')
 
 exports.post_create_get = async (req, res) => {
   try {
-    res.render("posts/new.ejs")
+    res.render('posts/new.ejs')
   } catch (error) {
-    console.error("An error has occurred while directing user!", error.message)
+    console.error('An error has occurred while directing user!', error.message)
   }
 }
 
@@ -17,40 +17,51 @@ exports.post_create_post = async (req, res) => {
     const post = await Post.create({
       description: req.body.description,
       picture: req.file.filename,
-      user: user._id,
+      user: user._id
     })
 
     res.redirect(`/posts/${post._id}`)
   } catch (error) {
-    console.error("An error has occurred creating a post!", error.message)
+    console.error('An error has occurred creating a post!', error.message)
   }
 }
 
 exports.post_index_get = async (req, res) => {
   try {
-    const posts = await Post.find().populate("user")
+    const posts = await Post.find().populate('user')
+    let userHasFavorited = false
     posts.forEach((post) => {
-      console.log(post.user.username)
+      userHasFavorited = post.favorited.some(
+        (user) => user === req.session.user._id
+      )
     })
-    res.render("posts/index.ejs", { posts, showComments: false })
+
+    res.render('./posts/index.ejs', {
+      user: req.session.user,
+      posts,
+      showComments: false,
+      userHasFavorited
+    })
+    //  res.render("posts/index.ejs", { posts, showComments: false })
   } catch (error) {
-    console.error("An error has occurred while viewing posts!", error.message)
+    console.error('An error has occurred while viewing posts!', error.message)
   }
 }
 
 exports.post_show_get = async (req, res) => {
   try {
-    const post = await Post.findById(req.params.postId).populate("user")
-    
+    const post = await Post.findById(req.params.postId).populate('user')
+
     const comments = await Comment.find({ post: req.params.postId }).populate(
-      "user"
+      'user'
     )
-    const userHasFavorited = post.favorited.some((user) =>
-      user === req.session.user._id
-    );
-    const numberOfFollowers = post.favorited.length 
+    let userHasFavorited = false
+    userHasFavorited = post.favorited.some(
+      (user) => user === req.session.user._id
+    )
+    const numberOfFollowers = post.favorited.length
     post.comments = comments
-    res.render("./posts/show.ejs", {
+    res.render('./posts/show.ejs', {
       user: req.session.user,
       post,
       comments,
@@ -69,10 +80,10 @@ exports.post_show_get = async (req, res) => {
 exports.post_edit_get = async (req, res) => {
   try {
     const post = await Post.findById(req.params.postId)
-    res.render("./posts/edit.ejs", { post })
+    res.render('./posts/edit.ejs', { post })
   } catch (error) {
     console.error(
-      "An error has occurred while directing to edit view!",
+      'An error has occurred while directing to edit view!',
       error.message
     )
   }
@@ -83,7 +94,7 @@ exports.post_update_put = async (req, res) => {
     const post = await Post.findByIdAndUpdate(req.params.postId, req.body)
     res.redirect(`/posts/${post._id}`)
   } catch (error) {
-    console.error("An error has occurred updating a post!", error.message)
+    console.error('An error has occurred updating a post!', error.message)
   }
 }
 exports.post_delete_delete = async (req, res) => {
@@ -91,7 +102,7 @@ exports.post_delete_delete = async (req, res) => {
     await Post.findByIdAndDelete(req.params.postId)
     res.redirect(`/users/${req.session.user._id}`)
   } catch (error) {
-    console.error("An error has occurred deleting a post!", error.message)
+    console.error('An error has occurred deleting a post!', error.message)
   }
 }
 
@@ -102,20 +113,20 @@ exports.comment_create_post = async (req, res) => {
     const comment = await Comment.create({
       post: req.params.postId,
       description: req.body.comment,
-      user: req.session.user._id,
+      user: req.session.user._id
     })
 
     res.redirect(`/posts/${req.params.postId}`)
   } catch (error) {
-    console.error("An error has occurred creating a comment!", error.message)
+    console.error('An error has occurred creating a comment!', error.message)
   }
 }
 exports.comment_delete_delete = async (req, res) => {
   try {
     await Comment.findByIdAndDelete(req.params.commentId)
-    res.redirect("/posts/")
+    res.redirect('/posts/')
   } catch (error) {
-    console.error("An error has occurred creating a comment!", error.message)
+    console.error('An error has occurred creating a comment!', error.message)
   }
 }
 
@@ -123,11 +134,10 @@ exports.likes_create_post = async (req, res) => {
   try {
     await Post.findByIdAndUpdate(req.params.postId, {
       $addToSet: { favorited: req.params.userId }
-      
     })
     res.redirect(`/posts/${req.params.postId}`)
   } catch (error) {
     console.log(error)
-    res.redirect("/")
+    res.redirect('/')
   }
 }
